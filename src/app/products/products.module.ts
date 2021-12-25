75f2970afb53841f1e1b7904dc521a2a5beb7196
imankns70 <45065474+imankns70@users.noreply.github.com>
2021-12-25 22:59:58 +0330

    add store to products

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,6 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
+
+import { reducers } from './store';
+
 // components
 import * as fromComponents from './components';
 
@@ -35,9 +39,10 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
+    StoreModule.forFeature('prodcuts', reducers)
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
-export class ProductsModule {}
+export class ProductsModule { }
